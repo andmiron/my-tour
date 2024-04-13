b9758a6a886786b24987be7762edf7e72e1f2667
Andrii Myronenko <sylneyshii@gmail.com>
2024-04-14 01:23:09 +0300

    refactored profile template

--- models/user.model.js
+++ models/user.model.js
@@ -10,10 +10,6 @@ const userSchema = new mongoose.Schema(
          required: [true, 'Email is required!'],
          lowercase: true,
       },
-      emailConfirmed: {
-         type: Boolean,
-         default: false,
-      },
       firstName: {
          type: String,
       },
